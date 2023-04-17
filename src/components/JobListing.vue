fa92fd9f6dbff7888810b2ba7673f3666a323011
Jhona Rica Manahan <manhan.jrd@gmail.com>
2023-04-17 20:39:31 +0800

    setup vue-bootstrap

---
+++ src/components/JobListing.vue
@@ -0,0 +1,15 @@
+<script>
+      import json from '../json/data.json'
+      export default{
+          data(){
+              return{
+                  myJson: json
+              }
+          }
+      }
+</script>
+
+<template>
+  <h1>JOBLISTING</h1>
+  <p>data{{ myJson[1] }}</p>
+</template>
