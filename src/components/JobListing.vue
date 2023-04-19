cc01a72abc1465c0ba35ef10a040c265f5ad4a8f
Jhona Rica Manahan <manhan.jrd@gmail.com>
2023-04-19 20:26:02 +0800

    render-data

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -3,7 +3,7 @@
       export default{
           data(){
               return{
-                  myJson: json
+                  dataJson: json
               }
           }
       }
@@ -11,5 +11,19 @@
 
 <template>
   <h1>JOBLISTING</h1>
-  <p>data{{ myJson[1] }}</p>
+  <ul class="list-unstyled">
+    <li v-for="dataJson in dataJson" :key="dataJson.id" class="border border-danger mb-2">
+      <div class="d-flex">
+        <img :src="dataJson.logo" alt="SVG logo">
+        {{ dataJson.company }}
+        <p v-if="dataJson.new == true">New!</p>
+        <p v-if="dataJson.featured == true">Featured!</p>
+      </div>
+        <p>{{ dataJson.position }}</p>
+      <div>{{ dataJson.postedAt }} · {{ dataJson.contract }} · {{ dataJson.location }}</div>
+      <span v-for="languages in dataJson.languages" :key="languages" class="mx-2">
+        {{ languages }}
+      </span>
+    </li>
+  </ul>
 </template>
